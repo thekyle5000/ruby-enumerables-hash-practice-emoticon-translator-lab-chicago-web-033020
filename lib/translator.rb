e41e662d1e65e86c2327ce9df579c9088f3340b3
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-29 21:28:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,22 +5,28 @@ require 'yaml'
 def load_library(file_path)
 db = YAML.load_file(file_path)
 meaning_hash = {}
-meaning_hash
-
-h = {}
-h["get_meaning"] = db.each do |k, v|
-  h_inner = {}
-  h_inner[db[v][1]] = db[k]
-  binding.pry
+db.each do |k, v|
+  meaning_hash[db[k][1]] = db[k]
 end
-h["get_emoitcon"] = db.each do |key, value|
-
-end
-
-end
+binding.pry
+  
+  
   
 
-end
+# h = {}
+# h["get_meaning"] = db.each do |k, v|
+#   h_inner = {}
+#   h_inner[db[v][1]] = db[k]
+#   binding.pry
+# end
+# h["get_emoitcon"] = db.each do |key, value|
+
+# end
+
+# end
+  
+
+# end
 
 def get_japanese_emoticon
   # code goes here
