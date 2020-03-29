603eddcab5a80c64b4895470552c9f76d98fe3fd
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-29 21:05:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,12 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+db = YAML.load_file(file_path)
+get_meaning = db.map do |k, v| 
+  
+
 end
 
 def get_japanese_emoticon
