a52379956d01ae35e80a3bb59c136b3314bfaa19
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-29 22:05:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,6 +38,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  my_db = load_libary(file_path)
   binding.pry
+  my_db = load_libary(file_path)
+
 end
