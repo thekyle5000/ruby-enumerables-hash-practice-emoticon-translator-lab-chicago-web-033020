58ad93f073d411e581558d633961a2232f54a7b3
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-29 22:11:25 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,54 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+db = YAML.load_file(file_path)
+the_hash = {}
+
+meaning_hash = {}
+db.each do |k, v|
+meaning_hash[db[k][1]] = k
+end
+the_hash["get_meaning"] = meaning_hash
+
+emoticon_hash = {}
+db.each do |key, value|
+  emoticon_hash[db[key][0]] = db[key][1]
+end
+the_hash["get_emoticon"] = emoticon_hash
+the_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(file_path, emoticon)
+  my_db = load_library(file_path)
+  je = nil
+  my_db["get_emoticon"].each do |k, v|
+  if k == emoticon
+    je = v
+  end
+  end
+  if je == nil
+    final = "Sorry, that emoticon was not found"
+  else 
+    final = je
+  end
+  final
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  my_db = load_library(file_path)
+  value = nil
+  my_db["get_meaning"].each do |k, v|
+   if k == emoticon
+    value = v
+   end
+  end
+  if value == nil
+    final = "Sorry, that emoticon was not found"
+  else
+    final = value
+  end
+  final
 end
